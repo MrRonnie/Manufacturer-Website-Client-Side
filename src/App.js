0fc2b446ecdae2448d8cca6dd96ffa7c34b1828a
MrRonnie <ronymaza55@gmail.com>
2022-05-23 14:25:16 +0600

    Naver section done and made some routes and likes to the pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route, Link } from "react-router-dom";
 import Navbar from "./Pages/Shared/Navbar";
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
