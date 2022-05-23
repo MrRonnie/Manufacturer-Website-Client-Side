49945d88bdd943a670ebc8e063cdadffc6e43ad6
MrRonnie <ronymaza55@gmail.com>
2022-05-23 21:02:41 +0600

    Add a common button

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: "#a991f7",
-          secondary: "#f6d860",
-          accent: "#37cdbe",
+          primary: "#19D3AE",
+          secondary: "#0FCFEC",
+          accent: "#3A4256",
           neutral: "#3d4451",
           "base-100": "#ffffff",
         },
